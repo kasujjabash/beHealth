dc37277a4645aedf5d0f5173a8e29b26981d5a51
kasujjabash <kasujjabash80@gmail.com>
2024-06-06 20:51:38 +0300

    New ui update

--- lib/screens/wallet.dart
+++ lib/screens/wallet.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Wallet extends StatelessWidget {
-  const Wallet({super.key});
+  const Wallet({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
